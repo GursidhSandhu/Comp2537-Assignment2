9441ad5b07db076a84b8c9c7531e660e12a6a033
GursidhSandhu <gsandhu168@my.bcit.ca>
2023-05-08 15:08:04 -0700

    finished

--- index.js
+++ index.js
@@ -27,6 +27,18 @@ const Joi = require('joi');
 // allows us to use parsed data from form to request body
 app.use(express.urlencoded({extended:false}));
 
+// telling express we want to use render EJS files
+app.set('view engine', 'ejs');
+
+// links for the header navbar
+const navLinks = [
+    {name: 'Home', link: '/'},
+    {name: 'Login', link: '/login'},
+    {name: 'Members', link: '/members'},
+    {name: 'Admin', link: '/admin'},
+    {name: '404', link: '/*'}
+]
+
 // how long the session is valid for : 1 hour
 const expireTime = 1* 60 * 60 * 1000;
 
@@ -61,6 +73,44 @@ app.use(session({
 }
 ));
 
+// method that checks if a session is valid
+function isValidSession(req) {
+    if(req.session.authenticated){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// middleware function
+function sessionValidation(req,res,next){
+    if(isValidSession(req)){
+        next();
+    } else {
+        res.redirect('/');
+    }
+}
+
+// method that checks if an user type is admin
+function isAdmin(req) {
+    if (req.session.user_type == 'admin') {
+        return true;
+    }
+    return false;
+}
+
+// middleware function
+function adminAuthorization(req, res, next) {
+    if (!isAdmin(req)) {
+        res.status(403);
+        res.render("errorMessage", {error: "Not Authorized"});
+        return;
+    }
+    else {
+        next();
+    }
+}
+
 // creating a home page for users to either click login or signup
 app.get('/', (req,res) => {
 
@@ -68,20 +118,10 @@ app.get('/', (req,res) => {
     if (req.session.authenticated) {
          // local variable to hold username of current session
         var username = req.session.username;
-
-        var html = `
-        <h2> Hello ${username}! </h2>
-        <a href='/members'><button> Go to Members Area </button></a><br>
-        <a href='/logout'><button> Logout </button></a>`
-        res.send(html);
+        res.render('loggedIn', {username: username, navLinks: navLinks});
         return;
     }
-
-    var html = `
-    <h1> Welcome to Gursidh's Website!</h1>
-    <a href='/login'><button>Login</button></a><br>
-    <a href='/signup'><button>Signup</button></a>`;
-    res.send(html);
+    res.render('home', {navLinks: navLinks});
 });
 
 // method to check nosql injection attacks
@@ -112,14 +152,7 @@ app.get('/nosql-injection', async (req,res) => {
 
 // creating a page for users to login
 app.get('/login', (req,res) => {
-    var html = `
-    <h2> log in </h2>
-    <form action = '/loginSubmit' method = 'post'>
-    <input name = 'email' type = 'email' placeholder = 'email'><br>
-    <input name = 'password' type = 'password' placeholder = 'password'><br>
-    <button>Submit</button>
-    </form>`;
-    res.send(html);
+    res.render('login', {navLinks: navLinks});
 });
 
 // check if the attempt to login is valid or not
@@ -134,23 +167,17 @@ app.post('/loginSubmit', async(req,res) => {
     
     // if the email is not valid then redirect user to login again
 	if (validationResult.error != null) {
-        var html = `
-        <h2> Invalid email.</h2>
-        <a href='/login'><h2> try again </h2></a>`;
-        res.send(html);
+        res.render('loginAttempt1', {navLinks: navLinks});
 	   return;
     }
 
     // find the user in the database and store in result variable
-    const result = await userCollection.find({email: email}).project({username: 1, password: 1, _id: 1}).toArray();
+    const result = await userCollection.find({email: email}).project({username: 1, password: 1, user_type: 1, _id: 1}).toArray();
 
     // redirect user to login if the email is not found in database 
     // if the email pops up more than once then user will be prompted to login again
 	if (result.length != 1) {
-        var html = `
-        <h2> Invalid email/password combination.</h2>
-        <a href='/login'><h2> try again </h2></a>`;
-        res.send(html);
+        res.render('loginAttempt2', {navLinks: navLinks});
 		return;
 	}
     // if email found then compare the given password to stored password
@@ -160,32 +187,23 @@ app.post('/loginSubmit', async(req,res) => {
 		req.session.authenticated = true;
 		req.session.email = email;
         req.session.username = result[0].username;
+        req.session.user_type = result[0].user_type;
 		req.session.cookie.maxAge = expireTime;
-
-		res.redirect('/members');
+        
+		res.render('members', {username: req.session.username, navLinks: navLinks});
 		return;
     }
     // if email exists but passwords dont match then prompt user to login again.
 	else {
-        var html = `
-        <h2> Incorrect password.</h2>
-        <a href='/login'><h2> try again </h2></a>`;
-        res.send(html);
+        var html = 
+        res.render('loginAttempt3', {navLinks: navLinks});
 		return;
 	}
 });
 
 // creating a page for users to signup
 app.get('/signup', (req,res) => {
-    var html = `
-    <h2> create user </h2>
-    <form action = '/signupSubmit' method = 'post'>
-    <input name = 'username' type = 'text' placeholder = 'username'><br>
-    <input name = 'email' type = 'email' placeholder = 'email'><br>
-    <input name = 'password' type = 'password' placeholder = 'password'><br>
-    <button>Submit</button>
-    </form>`;
-    res.send(html);
+    res.render('signup', {navLinks: navLinks});
 });
 
 // action that happens when new user is create
@@ -205,65 +223,65 @@ app.post('/signupSubmit', async(req,res) => {
 
 	 const validationResult = schema.validate({username, email, password});
 
-
     // if validation throws an error then redirect user to signup page
 	 if (validationResult.error != null) {
 	   var error = validationResult.error;
-       var html = `
-       <h1> ${error} </h1>
-       <a href='/signup'><h2> Try again </h2></a>`;
-       res.send(html);
+       res.render('signupAttempt1', {error: error, navLinks: navLinks});
 	   return;
    } 
        // check if email already exists in the user collection
        var existingUser = await userCollection.findOne({ email: email });
 
    if (existingUser) {
-       var html = `
-           <h1> This email already exists. </h1>
-           <a href='/signup'><h2> Try again </h2></a>`;
-       res.send(html);
+    res.render('signupAttempt2', {navLinks: navLinks});
        return;
     } 
 
    // hash the inserted password
    var hashedPassword = await bcrypt.hash(password, saltRounds);
 
-    // add the new user to collection of users in database
-	await userCollection.insertOne({username: username, email: email, password: hashedPassword});
+if(username == 'gursidh' ){
+        // gursidh is the only user that has admin access
+        await userCollection.insertOne({username: username, email: email, password: hashedPassword, user_type: "admin"});
+        req.session.user_type = 'admin';
+} else {
+        // every other user is just a normal user
+	await userCollection.insertOne({username: username, email: email, password: hashedPassword, user_type: "user"});
+    req.session.user_type = 'user';
+}
 
     // if passes validation code, begin session and redirect to members page
     req.session.authenticated = true;
     req.session.cookie.maxAge = expireTime;
     req.session.username = username;
-    res.redirect('/members');
-    
+    res.render('members', {username: req.session.username, navLinks: navLinks});
+   
+});
 
+app.get('/changeToUser/:name', async(req,res) => {
+    await userCollection.updateOne({username: req.params.name}, {$set: {user_type: 'user'}});
+   // const result = await userCollection.find().project({username: 1, user_type: 1, _id: 1}).toArray();
+    res.redirect('/admin');
+});
+
+// method that changes user type to admin
+app.get('/changeToAdmin/:name', async(req,res) => {
+    await userCollection.updateOne( { username: req.params.name },{ $set: { user_type: 'admin' } });
+   // const result = await userCollection.find().project({username: 1, user_type: 1, _id: 1}).toArray();
+    res.redirect('/admin');
 });
 
 // members area page
+app.use('/members', sessionValidation);
 app.get('/members', (req,res) => {
+        res.render('members', {username: req.session.username,navLinks: navLinks});
+});
 
-    // check if current session is valid
-    if(req.session.authenticated){
-        // local variable to hold username of current session
-        var username = req.session.username;
-
-        // array of picture filenames
-        var pictures = ['C6-GIF.gif', 'C7-GIF.webp', 'C8-GIF.webp'];
-
-        // get a random index from the pictures array
-        var randomIndex = Math.floor(Math.random() * pictures.length);
-
-        var html = `
-        <h2> Hello ${username}! </h2>
-        <img src='/${pictures[randomIndex]}' alt="Random picture"><br>
-        <a href='/logout'><button> Logout </button></a>`;
-        res.send(html);
-    }else{
-        res.redirect('/');
-    }
-
+// making an admins page
+app.get('/admin', sessionValidation, adminAuthorization, async(req,res) => {
+    // store every user in an array
+    const result = await userCollection.find().project({username: 1, user_type: 1, _id: 1}).toArray();
+    res.render('admin', {users: result, navLinks: navLinks});
 });
 
 // page that represents a user is logged out
@@ -280,10 +298,9 @@ app.use(express.static(__dirname + "/public"));
 // catch any page that does not exist and make it a 404 page
 app.get("*", (req,res) => {
     res.status(404);
-    res.send("Page is not found or does not exist - 404");
+    res.render('404', {navLinks: navLinks});
 });
     
 app.listen(port, () => {
     console.log("Node application listening on port " + port);
 });
-
